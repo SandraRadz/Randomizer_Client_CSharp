e0585df6ad58d24c3e6c7f82d628dcad61e6cb1c
SandraRadz <oleksandraradzievska@gmail.com>
2019-11-13 20:17:17 +0200

    sign in, sign up design

---
+++ Models/Request.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Randomizer_Client.Models
+{
+    public class Request
+    {
+        public Guid Id { get; set; }
+        public int From { get; set; }
+        public int To { get; set; }
+        public int Count { get; set; }
+        public DateTime Time { get; set; }
+        public User ReqUser { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Randomizer_Client.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime LastAccess { get; set; }
+        public ICollection<Request> Requests { get; set; }
+    }
+}
